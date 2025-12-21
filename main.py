1ba6e7a84eb6b3d56b80f990ce0d20e62484f58a
Anastasija315 <anastasija.mezecka@gmail.com>
2025-12-21 14:04:51 +0200

    Update main.py
    Atjauninātas lietotāju mijedarbības opcijas (sastībā ar uzdevumā prasītām)

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ def main():
         print("Ievadiet skaitli no 1 līdz 5, lai izvēlēties darbību:")
         print("1 - Pievienot vai atjaunināt grāmatu")
         print("2 - Apskatīt visas grāmatas")
-        print("3 - Top 5 visvairāk izsniegtās grāmatas")
-        print("4 - Top 5 dārgākās grāmatas")
-        print("5 - Iziet")
+        print("3 - Aizņemt vai atgriezt grāmatu")
+        print("4 - Meklēt grāmatu")
+        print("5 - Apskatīt Top 5 sarakstus")
+        print("6 - Iziet no sistēmas")
 
         izvele = input("Jūsu izvēle: ")
 
@@ -17,14 +18,16 @@ def main():
         elif izvele == "2":
             show_all_books()
         elif izvele == "3":
-            top_5_popular()
+            pass
         elif izvele == "4":
-            top_5_expensive()
+            pass
         elif izvele == "5":
+            pass
+        elif izvele == "6":
             print("Uz redzēšanos!")
             break
         else:
-            print("Kļūda! Ievadiet skaitli no 1 līdz 5")
+            print("Kļūda! Ievadiet skaitli no 1 līdz 6")
 
 if __name__ == "__main__":
     main()
