05564c1ab9de905cd69100924e2ceb155e1925af
Anastasija315 <anastasija.mezecka@gmail.com>
2025-12-21 14:00:19 +0200

    Update book_manager.py
    Papildināta grāmatu pievienošanas / atjaunināšanas funkcionalitāte. Pievienotas kļūdu apstrādes un paziņojumi. Izveidotas funkcijas reitingiem (top 5 populārākas un top 5 dārgākās grāmatas).

--- book_manager.py
+++ book_manager.py
@@ -1,16 +1,21 @@
 import json
 
+dati = "data/library_data.json"
+
 def load_books():
     """nolasa grāmatu datus"""
   
-    with open("data/library_data.json", "r", encoding="utf-8") as file:
-        return json.load(file)
+    try:
+        with open(dati, "r", encoding="utf-8") as file:
+            return json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return []
 
 
 def save_books(books):
     """saglabā grāmatu datus"""
 
-    with open("data/library_data.json", "w", encoding="utf-8") as file:
+    with open(dati, "w", encoding="utf-8") as file:
         json.dump(books, file, ensure_ascii=False, indent=4)
 
 
@@ -18,30 +23,81 @@ def add_or_update_book():
     """atjaunina vai pievieno jaunu grāmatu"""
 
     books = load_books()
-    nosaukums = input("Nosaukums: ")
-    autors = input("Autors: ")
-    cena = float(input("Cena: "))
-    eksemplari = int(input("Pieejamais eksemplāru skaits: "))
+    nosaukums = input("Nosaukums: ").strip()
+    autors = input("Autors: ").strip()
+
+    try:
+        cena = float(input("Cena: "))
+        pieejamas = int(input("Pieejamais eksemplāru skaits: "))
+    except ValueError:
+        print("Kļūda! Cena un pieejamais grāmatu skaits jāievada kā skaitļi!")
+        return
+    
+    if cena < 0 or pieejamas < 0:
+        print("Kļūda! Cena un pieejamais grāmatu skaits nevar būt negatīvi!")
+        return
 
     # pārbauda, vai grāmata jau eksistē
     for book in books:
         if book["nosaukums"].lower() == nosaukums.lower():
             book["autors"] = autors
             book["cena"] = cena
-            book["eksemplari"] += eksemplari
+            book["pieejamas"] = pieejamas
             print("Grāmata ir atjaunināta")
             save_books(books)
             return
         
-        # ja neeksistē - pievieno jaunu
-        new_book = {
-          "nosaukums": nosaukums,
-          "autors": autors,
-          "cena": cena,
-          "eksemplari": eksemplari,
-          "izsniegtais": 0
-        }
+    # ja neeksistē - pievieno jaunu
+    new_book = {
+        "nosaukums": nosaukums,
+        "autors": autors,
+        "cena": cena,
+        "pieejamas": pieejamas,
+        "izsniegtas": 0
+    }
 
-        books.append(new_book)
-        save_books(books)
-        print("Grāmata ir pievienota")
+    books.append(new_book)
+    save_books(books)
+    print("Grāmata ir pievienota")
+
+
+def show_all_books():
+    """visu grāmatu saraksta apskate"""
+
+    books = load_books()
+    print("\n--- Visas pieejamas grāmatas ---")
+
+    if not books:
+        print("Nav grāmatu")
+        return
+
+    for book in books:
+        print("-----------------------------")
+        print(f"Nosaukums: {book['nosaukums']}")
+        print(f"Autors: {book['autors']}")
+        print(f"Cena: {book['cena']} EUR")
+        print(f"Pieejamas: {book['pieejamas']}")
+        print(f"Izsniegtas: {book['izsniegtas']}")
+        print("-----------------------------")
+
+
+def top_5_popular():
+    """top 5 visvairāk izsniegtās grāmatas"""
+
+    books = load_books()
+    print("\n--- Top 5 visvairāk izsniegtas grāmatas ---")
+    sorted_books = sorted(books, key=lambda x: x["izsniegtas"], reverse=True) # sakārto grāmatas pēc mainīgā "izsniegtas"
+
+    for book in sorted_books[:5]:
+        print(book["nosaukums"], "-", book["izsniegtas"])
+
+
+def top_5_expensive():
+    """top 5 dārgākās grāmatas"""
+
+    books = load_books()
+    print("\n--- Top 5 dārgākās grāmatas ---")
+    sorted_books = sorted(books, key=lambda x: x["cena"], reverse=True) # sakārto grāmatas pēc mainīgā "cena"
+
+    for book in sorted_books[:5]:
+        print(book["nosaukums"], "-", book["cena"], "EUR")
