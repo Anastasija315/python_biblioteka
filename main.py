593d1147958ab3a0ae9938208b9b793aba7c5115
Anastasija315 <anastasija.mezecka@gmail.com>
2025-12-27 12:10:55 +0200

    Update main.py
    Sakarā ar izmaiņām modulī book_manager.py, tika atjaunināts arī main.py modulis - tika nomainīts imports un metožu izsaukšana

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from book_manager import add_or_update_book, show_all_books, top_5_popular, top_5_expensive
+from book_manager import Library
 
 def main():
+    library = Library() 
+
     while True:
         print("\n *** Laipni lūgti digitālajā bibliotēkā! ***")
         print("Ievadiet skaitli no 1 līdz 5, lai izvēlēties darbību:")
@@ -14,15 +16,26 @@ def main():
         izvele = input("Jūsu izvēle: ")
 
         if izvele == "1":
-            add_or_update_book()
+            library.add_or_update_book()
         elif izvele == "2":
-            show_all_books()
+            library.show_all_books()
         elif izvele == "3":
             pass
         elif izvele == "4":
             pass
         elif izvele == "5":
-            pass
+            print("\n1 - Top 5 visvairāk izsniegtās grāmatas")
+            print("2 - Top 5 dārgākās grāmatas")
+            izvele2 = input("Izvēle: ")
+
+            if izvele2 == "1":
+                library.top_5_popular()
+            elif izvele2 == "2":
+                library.top_5_expensive()
+            else:
+                print("Kļūda! Ievadiet skaitli no 1 līdz 2")
+
+
         elif izvele == "6":
             print("Uz redzēšanos!")
             break
